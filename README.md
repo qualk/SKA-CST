28c5739a806d21965301426cd82d94cfb3602a23
qualk <qualk@qualk.xyz>
2024-08-14 12:58:24 +1000

    procrastinating

--- README.md
+++ README.md
@@ -1,16 +1,18 @@
 # SKA CSP - PSR Packet Analyser
 
 https://docs.google.com/document/d/1LpEoxHtj0BMfzKg3FEBXUUra2S6qtCVfocQqX1uXkHE/edit#heading=h.hx5ha7v7z3e7
+
 https://gitlab.com/ska-telescope/low-cbf/ska-low-cbf-integration
 
 ## How to Use
 
-Name your network capture `pss.pcap` (or change the value `payloads = extract_udp_payloads("pss.pcap")` on line 127 to whatever you want) and make sure it's in the same directory as the script `psr-analyzer.py`. Then run the script with `python psr-analyzer.py`, and it outputs a nice looking table, or an error.
+Name your network capture `pss.pcap` (or change the value `capture_file = "pss.pcap"` near the top of the file) and make sure it's in the same directory as the script `psr-analyzer.py`. Then run the script with `python psr-analyzer.py`.
 
 ### Arguments
 
-`--plain` or `--notable` output the plain output, without the table formatting.
-If you want to output the data to a file, just run it with `> file`. e.g.:
+`--plain` output the plain output, without the table formatting.
+
+If you want to output the data to a file, just run it with `> file`. Example command:
 ```bash
-python psr-analyzer.py --plain > analysis.txt
+python psr-analyser.py --plain > analysis.txt
 ```
--- psr-analyser.py
+++ psr-analyser.py
@@ -5,9 +5,10 @@ import sys
 from scapy.all import UDP, rdpcap
 from tabulate import tabulate
 
+capture_file = "pss.pcap"
 
 class PulsarPacketFormat:
-   packet_dest_mapping = {
+   packet_dest_map = {
       0: "Low PSS",
       1: "Mid PSS",
       2: "Low PST",
@@ -16,49 +17,56 @@ class PulsarPacketFormat:
    
    def __init__(self, payload: bytes):
       # Metadata
-      self.sequence_number = struct.unpack("<Q", payload[0:8])[0]  # Little-endian unsigned long long
+      self.n_sequence = struct.unpack("<Q", payload[0:8])[0]  # Little-endian unsigned long long
       self.timestamp_attoseconds = struct.unpack("<Q", payload[8:16])[0]  # Little-endian unsigned long long
       self.timestamp_seconds = struct.unpack("<I", payload[16:20])[0]  # Little-endian unsigned int
       self.channel_separation = struct.unpack("<I", payload[20:24])[0]  # Little-endian unsigned int
-      self.first_channel_frequency = struct.unpack("<Q", payload[24:32])[0]  # Little-endian unsigned long long
+      self.first_channel_freq = struct.unpack("<Q", payload[24:32])[0]  # Little-endian unsigned long long
       self.scale1 = struct.unpack("<f", payload[32:36])[0]  # Little-endian float
       self.scale2 = struct.unpack("<f", payload[36:40])[0]  # Little-endian float
       self.scale3 = struct.unpack("<f", payload[40:44])[0]  # Little-endian float
       self.scale4 = struct.unpack("<f", payload[44:48])[0]  # Little-endian float
-      self.first_channel_number = struct.unpack("<I", payload[48:52])[0]  # Little-endian unsigned int
+      self.n_first_channel = struct.unpack("<I", payload[48:52])[0]  # Little-endian unsigned int
       self.channels_per_packet = struct.unpack("<H", payload[52:54])[0]  # Little-endian unsigned short
       self.valid_channels_per_packet = struct.unpack("<H", payload[54:56])[0]  # Little-endian unsigned short
-      self.number_of_time_samples = struct.unpack("<H", payload[56:58])[0]  # Little-endian unsigned short
-      self.beam_number = struct.unpack("<H", payload[58:60])[0]  # Little-endian unsigned short
+      self.n_time_samples = struct.unpack("<H", payload[56:58])[0]  # Little-endian unsigned short
+      self.n_beam = struct.unpack("<H", payload[58:60])[0]  # Little-endian unsigned short
       self.magic_word = hex(struct.unpack("<I", payload[60:64])[0]) # Little-endian unsigned int to hex
-      self.packet_destination = self.packet_dest_mapping.get(payload[64], int) # Unsigned int to str from mapping
+      self.packet_dest = self.packet_dest_map.get(payload[64], int) # Unsigned int to str from mapping
       self.data_precision = payload[65]  # Unsigned char
-      self.number_power_samples_averaged = payload[66]  # Unsigned char
+      self.n_power_samples_averaged = payload[66]  # Unsigned char
       self.samples_per_weight = payload[67]  # Unsigned char
       self.oversampling_ratio_numerator = payload[68]  # Unsigned char
       self.oversampling_ratio_denominator = payload[69]  # Unsigned char
-      self.beamformer_version = struct.unpack("<H", payload[70:72])[0]  # Little-endian unsigned short
+      self.beamformer_ver = struct.unpack("<H", payload[70:72])[0]  # Little-endian unsigned short
       self.scan_id = struct.unpack("<Q", payload[72:80])[0]  # Little-endian unsigned long long
       self.offset1 = struct.unpack("<f", payload[80:84])[0]  # Little-endian float
       self.offset2 = struct.unpack("<f", payload[84:88])[0]  # Little-endian float
       self.offset3 = struct.unpack("<f", payload[88:92])[0]  # Little-endian float
       self.offset4 = struct.unpack("<f", payload[92:96])[0]  # Little-endian float
       
-      # weight_data_offset = 96
-      # weight_data_bytes = self.number_of_time_samples / self.samples_per_weight * 2 * self.channels_per_packet
-      # self._data_offset = weight_data_bytes + weights_offset
-      
+      weight_data_offset = 96
+      weight_data_bytes = int(self.n_time_samples / self.samples_per_weight * 2 * self.channels_per_packet)
+      self._data_offset = len(payload) - weight_data_offset - weight_data_bytes
 
 def extract_udp_payloads(pcap_file):
-   packets = rdpcap(pcap_file)
-   payloads = []
+   try:
+      with open(pcap_file, "rb") as file:
+         packets = rdpcap(file)
+   except FileNotFoundError:
+      print(f"Error: File '{pcap_file}' does not exist.")
+      sys.exit(1)
+   except PermissionError:
+      print(f"Error: File '{pcap_file}' cannot be read due to insufficient permissoins.")
+      sys.exit(1)
+   except Exception as e:
+      print(f"Error reading pcap file: {e}")
+      sys.exit(1)
+      
+   payloads = [
+      bytes(packet[UDP].payload) for packet in packets if UDP in packet
+   ]
 
-   for packet in packets:
-      if UDP in packet and packet[UDP].payload:
-         payload = bytes(packet[UDP].payload) 
-         payloads.append(payload)
-         # print(f"Payload size: {len(payload)} bytes")
-   
    if not payloads:
       print("No UDP packets found")
       sys.exit(1)
@@ -70,7 +78,7 @@ def check_magic_words(payloads):
    for payload in payloads:
       packet = PulsarPacketFormat(payload)
       magic_words.append(packet.magic_word)
-   
+
    if all(magic_word == "0xbeadfeed" for magic_word in magic_words):
       print("Magic Words matching!")
       return True
@@ -86,27 +94,27 @@ def print_metadata(payloads):
       counter += 1
       packet = PulsarPacketFormat(payload)
       data = [
-         ("Sequence Number", packet.sequence_number),
+         ("Sequence Number", packet.n_sequence),
          ("Timestamp Attoseconds", packet.timestamp_attoseconds),
          ("Timestamp Seconds", packet.timestamp_seconds),
          ("Channel Separation", packet.channel_separation),
-         ("First Channel Frequency", packet.first_channel_frequency),
+         ("First Channel Frequency", packet.first_channel_freq),
          ("Scale 1", packet.scale1),
          ("Scale 2", packet.scale2),
          ("Scale 3", packet.scale3),
          ("Scale 4", packet.scale4),
-         ("First Channel Number", packet.first_channel_number),
+         ("First Channel Number", packet.n_first_channel),
          ("Channels per Packet", packet.channels_per_packet),
          ("Valid Channels per Packet", packet.valid_channels_per_packet),
-         ("n Time Samples per Packet", packet.number_of_time_samples),
-         ("Beam Number", packet.beam_number),
+         ("n Time Samples per Packet", packet.n_time_samples),
+         ("Beam Number", packet.n_beam),
          ("Magic Word", packet.magic_word),
-         ("Packet Destination", packet.packet_destination),
+         ("Packet Destination", packet.packet_dest),
          ("Data Precision", packet.data_precision),
-         ("n Power Samples Avg", packet.number_power_samples_averaged),
+         ("n Power Samples Avg", packet.n_power_samples_averaged),
          ("Samples per Weight", packet.samples_per_weight),
          ("Oversampling Ratio", f"{packet.oversampling_ratio_numerator}/{packet.oversampling_ratio_denominator}"),
-         ("Beamformer Version", packet.beamformer_version),
+         ("Beamformer Version", packet.beamformer_ver),
          ("Scan ID", packet.scan_id),
          ("Offset 1", packet.offset1),
          ("Offset 2", packet.offset2),
@@ -114,7 +122,7 @@ def print_metadata(payloads):
          ("Offset 4", packet.offset4),
       ]
       print(f"Packet Number #{counter}:")
-      if "--plain" in sys.argv or "--notable" in sys.argv:
+      if "--plain" in sys.argv:
          for item in data:
             print(f"{item[0]}: {item[1]}")
          for item in data:
@@ -124,7 +132,7 @@ def print_metadata(payloads):
       print()
       print()
 
-payloads = extract_udp_payloads("pss.pcap")
+payloads = extract_udp_payloads(capture_file)
 if check_magic_words(payloads) == True:
    print()
    print_metadata(payloads)
